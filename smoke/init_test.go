466e3f69b358d0c02da70718b1adff29fd5d5cb1
Michael Dawson <mdawson@devrus.com>
2023-06-09 16:37:46 -0400

    feat: initial version
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

---
+++ smoke/init_test.go
@@ -0,0 +1,34 @@
+package smoke_test
+
+import (
+	"flag"
+	"testing"
+	"time"
+
+	"github.com/onsi/gomega/format"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+
+	. "github.com/onsi/gomega"
+)
+
+var Builder string
+
+func init() {
+	flag.StringVar(&Builder, "name", "", "")
+}
+
+func TestSmoke(t *testing.T) {
+	format.MaxLength = 0
+	Expect := NewWithT(t).Expect
+
+	flag.Parse()
+
+	Expect(Builder).NotTo(Equal(""))
+
+	SetDefaultEventuallyTimeout(60 * time.Second)
+
+	suite := spec.New("Smoke", spec.Parallel(), spec.Report(report.Terminal{}))
+	suite("Node.js", testNodejs)
+	suite.Run(t)
+}
